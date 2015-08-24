0e661e64d643fb69d94ce97de9d519e26920edd2
unknown <Anant@RC-5CB420159J.rebuscode.com>
2015-08-24 15:02:36 +0530

    Init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rcDimpleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rcDimpleApp
+ */
+angular.module('rcDimpleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
