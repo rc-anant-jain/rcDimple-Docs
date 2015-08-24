9e6fdd9675084a00f3d519aebe07e9e5f3c1e077
rc-anant-jain <anant.j@rebuscode.com>
2015-08-24 19:10:31 +0530

    Tabs

---
+++ app/scripts/controllers/apidocs.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rcDimpleApp.controller:ApidocsCtrl
+ * @description
+ * # ApidocsCtrl
+ * Controller of the rcDimpleApp
+ */
+angular.module('rcDimpleApp')
+  .controller('ApidocsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
